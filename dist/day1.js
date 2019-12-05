b6f8ad1120141c089d7a273c101f82f4eea2ae76
Debbie Cheng <dqrcheng@gmail.com>
2019-12-05 21:43:31 +1100

    day1part2

--- dist/day1.js
+++ dist/day1.js
@@ -5,24 +5,45 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = __importDefault(require("fs"));
 class Day1 {
+    calc(mass) {
+        return Math.floor(mass / 3) - 2;
+    }
     method1(data) {
         const massArray = data.split(/\r?\n/).map(Number);
         let totalFuel = 0;
         for (const i of massArray) {
-            totalFuel += (Math.floor((i / 3)) - 2);
+            totalFuel += this.calc(i);
         }
         return totalFuel;
     }
     method2(data) {
         return data.split(/\r?\n/)
             .map(Number)
-            .map(mass => Math.floor(mass / 3) - 2)
+            .map(x => this.calc(x))
+            .reduce((a, b) => a + b);
+    }
+    calcActualFuel(mass) {
+        let m = mass;
+        let sum = 0;
+        while (m > 0) {
+            m = this.calc(m);
+            sum += Math.max(m, 0);
+            console.log("current mass: " + m + " sum: " + sum);
+        }
+        return sum;
+    }
+    method3(data) {
+        return data.split(/\r?\n/)
+            .map(Number)
+            .map(x => this.calcActualFuel(x))
             .reduce((a, b) => a + b);
     }
     run() {
         const data = fs_1.default.readFileSync('data/day1.txt', 'utf8');
         console.log(this.method1(data));
         console.log(this.method2(data));
+        console.log(this.calcActualFuel(14));
+        console.log(this.method3(data));
     }
 }
 exports.Day1 = Day1;
--- src/day1.ts
+++ src/day1.ts
@@ -1,11 +1,15 @@
 import fs from "fs";
 
 export class Day1 {
+    calc(mass: number): number {
+        return Math.floor(mass / 3) - 2
+    }
+
     method1(data: string): number {
         const massArray: Array<number> = data.split(/\r?\n/).map(Number);
         let totalFuel = 0
         for (const i of massArray) {
-            totalFuel += (Math.floor((i / 3)) - 2);
+            totalFuel += this.calc(i);
         }
         return totalFuel;
     }
@@ -13,7 +17,25 @@ export class Day1 {
     method2(data: string): number {
         return data.split(/\r?\n/)
             .map(Number)
-            .map(mass => Math.floor(mass / 3) - 2)
+            .map(x => this.calc(x))
+            .reduce((a, b) => a + b);
+    }
+
+    calcActualFuel(mass: number): number { 
+        let m = mass
+        let sum = 0
+        while (m > 0) {
+            m = this.calc(m);
+            sum += Math.max(m, 0);
+            console.log("current mass: " + m + " sum: " + sum);
+        }
+        return sum;
+    }
+
+    method3(data: string): number {
+        return data.split(/\r?\n/)
+            .map(Number)
+            .map(x => this.calcActualFuel(x))
             .reduce((a, b) => a + b);
     }
 
@@ -21,5 +43,8 @@ export class Day1 {
         const data: string = fs.readFileSync('data/day1.txt', 'utf8');
         console.log(this.method1(data));
         console.log(this.method2(data));
+        console.log(this.calcActualFuel(14));
+        console.log(this.method3(data));
     }
 }
+
